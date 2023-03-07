23afde7f846e8df4d4cb4bfbe86c6d3adf90999c
kyo2bay <kyo2bay@gmail.com>
2023-03-07 23:45:20 +0900

    feat: preview

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>kyo2bay preview test</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
